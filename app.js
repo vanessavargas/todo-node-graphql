2693b846942fabc9fbfd491bd2b375c8220140a1
Vanessa Vargas <vanessa.vargas4f@gmail.com>
2023-10-02 15:26:05 -0300

    Implementando graphql

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const schema = require('./schema');
 
-app.get('/', (req, res) => { 
-   res.send('Hello World!') 
- })  
+app.use(express.json());
+
+app.get("/graphql", (req, res) => {
+  res.json({
+    schema: schema,
+  });
+});
+
+app.post("/graphql", (req, res) => {
+  const body = req.body;
+
+  const result = graphql(schema, body.query, {
+    context: async () => ({
+      db: await connectDB(),
+    }),
+  });
+
+  res.json(result);
+});
  
 app.listen(port, () => {
    console.log(`Escutando em http://localhost:${port}`);
